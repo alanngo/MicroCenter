cbf61effc03a97e3598aaf2d5ea731e889139400
Alan <alan@remote.com>
2020-09-01 05:07:22 -0500

    added utils for heavy lifting

---
+++ src/main/java/com/microcenter/util/Utils.java
@@ -0,0 +1,38 @@
+package com.microcenter.util;
+
+import com.microcenter.dto.LaptopDTO;
+import com.microcenter.entity.Laptop;
+
+public class Utils
+{
+    public static LaptopDTO getDTOFrom(Laptop laptop)
+    {
+        return new LaptopDTO
+                (
+                        laptop.getId(),
+                        laptop.getName(),
+                        laptop.getCpu(),
+                        laptop.getRam(),
+                        laptop.getNvme(),
+                        laptop.getSsd(),
+                        laptop.getHdd(),
+                        laptop.getGpu()
+                );
+    }
+
+    public static Laptop getEntityFrom(LaptopDTO laptopDTO)
+    {
+        Laptop laptop = new Laptop();
+        if (laptopDTO!=null)
+        {
+            laptop.setName(laptopDTO.getName());
+            laptop.setCpu(laptopDTO.getCpu());
+            laptop.setRam(laptopDTO.getRam());
+            laptop.setNvme(laptopDTO.getNvme());
+            laptop.setSsd(laptopDTO.getSsd());
+            laptop.setHdd(laptopDTO.getHdd());
+            laptop.setGpu(laptopDTO.getGpu());
+        }
+        return laptop;
+    }
+}
