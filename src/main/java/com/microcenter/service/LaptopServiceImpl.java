00dfc8c0a319b436461fa0a3f593648700cf3fdc
Alan <alan@remote.com>
2020-09-03 10:46:40 -0500

    close #6

--- src/main/java/com/microcenter/service/LaptopServiceImpl.java
+++ src/main/java/com/microcenter/service/LaptopServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
