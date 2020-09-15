634c88103c9486e6348ee785790670cf35f791dd
Alan <alan@remote.com>
2020-09-15 00:10:58 -0500

    cross origin

--- src/main/java/com/microcenter/api/LaptopApi.java
+++ src/main/java/com/microcenter/api/LaptopApi.java
@@ -9,20 +9,14 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/laptops")
+@CrossOrigin
 @Validated
 public class LaptopApi
 {
