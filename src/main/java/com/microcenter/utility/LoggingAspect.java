1ccc65c83aee3848ea9d4d7761795ce767ea548d
Alan Ngo <alanngo673@gmail.com>
2020-09-18 18:52:40 -0500

    Update LoggingAspect.java

--- src/main/java/com/microcenter/utility/LoggingAspect.java
+++ src/main/java/com/microcenter/utility/LoggingAspect.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LoggingAspect
 {
-    private static final Log LOGGER = LogFactory.getLog(ExceptionControllerAdvice.class);
+    private static final Log LOGGER = LogFactory.getLog(LoggingAspect.class);
 
     @AfterThrowing(pointcut = "execution(* com.microcenter.service.*Impl.*(..))",throwing = "exception")
     public void log(Exception exception) { LOGGER.error(exception.getMessage(), exception); }
